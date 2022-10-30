78da964f469f2a1d8e216d8f1982bd23b46c9152
fly-body <1773617797@qq.com>
2022-10-30 15:10:44 +0800

    上线测试

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/lyq.headlines.github.io/'//这里写展示页面的仓库名称
+    : '/',
+    outputDir: "dist", // 输出文件目录
+    assetsDir: "static"//静态资源文件名称
+}
